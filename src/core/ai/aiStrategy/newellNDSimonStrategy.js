53f1dd8c5fa21475b71d0eb5fa6a783d069bfcac
LakhderAmine99 <amine.lakhder@usmba.ac.ma>
2022-12-14 19:41:59 +0100

    #add : NewellNdSimon strategy module to the ai sys

---
+++ src/core/ai/aiStrategy/newellNDSimonStrategy.js
@@ -0,0 +1,16 @@
+import AIStrategy from './AIStrategy.js';
+
+class NewellNDSimonStrategy extends AIStrategy {
+
+    constructor(){}
+
+    /**
+     * 
+     * @override
+     * @param {[[]]} board The current game board.
+     * @returns
+     */
+    predictNextMove(board){
+        return;
+    }
+}

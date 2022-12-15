2214ba104097e1a836d947fce714f22f956e0c82
LakhderAmine99 <amine.lakhder@usmba.ac.ma>
2022-12-15 17:04:16 +0100

    #add : add game combo cases matrix

--- src/core/game/gameState.js
+++ src/core/game/gameState.js
@@ -27,3 +27,14 @@ export const GameStrategies = {
     ATTACKING:0,
     DEFENDING:1
 };
+
+export const GameCombo = [
+    [0,1,2],
+    [0,3,6],
+    [0,4,8],
+    [3,4,5],
+    [1,4,7],
+    [2,5,8],
+    [6,7,8],
+    [2,4,6]
+];

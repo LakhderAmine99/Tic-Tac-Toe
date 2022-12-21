f99defaf89f81df82a069f383ff52bda70cf6382
LakhderAmine99 <amine.lakhder@usmba.ac.ma>
2022-12-21 23:44:27 +0100

    #update : update the ai exported modules

--- src/core/ai/aiStrategy/@index.js
+++ src/core/ai/aiStrategy/@index.js
@@ -1,9 +1,13 @@
 import RandomStrategy from "./randomStrategy.js";
 import DecisionTreeStrategy from "./decisionTreeStrategy.js";
-import NewellNDSimonStrategy from "./newellNDSimonStrategy.js"
+import NewellNDSimonStrategy from "./newellNDSimonStrategy.js";
+import AlphaBetaPruningStrategy from "./alphaBetaPruningStrategy.js";
+import MinimaxStrategy from "./minimaxStrategy.js";
 
 export {
     RandomStrategy,
     DecisionTreeStrategy,
-    NewellNDSimonStrategy
+    NewellNDSimonStrategy,
+    MinimaxStrategy,
+    AlphaBetaPruningStrategy
 };
